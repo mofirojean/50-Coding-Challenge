4d092820a6c611fd732fe05a10562968cb833716
Mofiro Jean <mofirojean@gmail.com>
2022-04-24 19:12:46 +0100

    just completed the 10th challenge in the list

---
+++ Java/src/com/mofirojean/Challenge10.java
@@ -0,0 +1,22 @@
+package com.mofirojean;
+
+public class Challenge10 {
+    // @author: mofirojean
+    // @CodeChallenge 10: Calculate the sum of numbers in an array of numbers
+
+    public static void main(String[] args){
+        // implements the array addition method
+        int[] arrayN = {2, 3, -1, 5, 7, 9, 10, 15, 95};
+        int total = arraySum(arrayN);
+        System.out.println(total);
+
+    }
+
+    // method to sum the elements of an array
+    public static int arraySum(int[] arrayNumber){
+        int total = 0;
+        for (int i: arrayNumber)
+            total += i;
+        return total;
+    }
+}
