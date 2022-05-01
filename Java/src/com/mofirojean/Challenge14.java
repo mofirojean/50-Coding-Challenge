4a50b10c09083f2fe9f831a890cda041ec572dea
Seige <kimbifavour@gmail.com>
2022-05-01 03:07:12 +0100

    Coding Challenge #14

---
+++ Java/src/com/mofirojean/Challenge14.java
@@ -0,0 +1,25 @@
+package com.mofirojean;
+
+public class Challenge14 {
+    //@author: kimbifavour
+    //codeChallenge 14: Print the first 10 fibonacci numbers without using recurssion
+
+    public static void main(String[] args){
+        fibonacci();
+    }
+
+    public static void fibonacci() {
+        int firstNumber = 0; // define the initial values of any fibonacci series
+        int secondNumber = 1;
+        int count = 1;
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
+        while (count < 8) {
+            int number = firstNumber + secondNumber; //the new number in the series is the sum of the previous two numbers
+            firstNumber = secondNumber; // the new values of first and second numbers are set
+            secondNumber = number;
+            System.out.println(number);
+            count++;
+        }
+    }
+}
