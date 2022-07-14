05414d1264533c49e24ca63e68eb258e1c52c28e
Mofiro Jean <mofirojean@gmail.com>
2022-07-14 01:35:11 +0100

    feat: completed Exercise17.py

---
+++ Python/Exercise17.py
@@ -0,0 +1,17 @@
+"""
+@author: mofirojean
+@project 16: Calculate the sum of digits
+of a positive integer number
+"""
+
+
+def sumDigits(n):
+    string = str(n)
+    total = 0
+    for i in string:
+        total += int(i)
+    return total
+
+
+digits = sumDigits(1226252)
+print("Sum:", digits)
