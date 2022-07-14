1a451d747d532297377c660f898aa187284a3213
Mofiro Jean <mofirojean@gmail.com>
2022-07-14 03:10:10 +0100

    feat: completed Exercise21.py

---
+++ Python/Exercise21.py
@@ -0,0 +1,29 @@
+"""
+@author: mofirojean
+@project 21: Rotate an array to the right 1 position
+"""
+
+
+# Function to right-rotate a list by one position
+def rightRotateByOne(arr):
+    last = arr[len(arr) - 1]
+    for i in range(len(arr) - 1, -1, -1):
+        arr[i] = arr[i - 1]
+    arr[0] = last
+
+
+# Function to left-rotate a list by `r` positions
+def rightRotate(arr, r):
+    # base case: invalid input
+    if r < 0 or r >= len(arr):
+        return
+
+    for i in range(r):
+        rightRotateByOne(arr)
+
+
+# Driver method
+myArr = [1, 2, 3, 4, 5]
+r = 2
+rightRotate(myArr, r)
+print(myArr)
