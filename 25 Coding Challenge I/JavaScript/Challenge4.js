340af6faf35f611242c512c37d2bb1b6f71fe03a
Mofiro Jean <mofirojean@gmail.com>
2022-08-05 13:39:59 +0100

    implemented challenge 4

---
+++ 25 Coding Challenge I/JavaScript/Challenge4.js
@@ -0,0 +1,13 @@
+// Challenge 4: Print the multiplication tables with numbers from 1 to 10
+
+function multiply(num){
+    for (let i = 1; i <= 10; i++){
+        let table  =  num + " * " + i + " = " + (i * num);
+        console.log(table);
+    }
+}
+
+for (let i = 1; i <= 10; i++){
+    console.log("Multiplication table for " + i)
+    multiply(i);
+}
