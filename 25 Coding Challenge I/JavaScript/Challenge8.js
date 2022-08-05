e8862f44235ef8baa77a677e4816efab3663c96c
Mofiro Jean <mofirojean@gmail.com>
2022-08-05 15:00:51 +0100

    implemented challenge 8

---
+++ 25 Coding Challenge I/JavaScript/Challenge8.js
@@ -0,0 +1,11 @@
+/**
+ * Challenge 8:
+ * Create a function that will convert from Celsius to Fahrenheit
+ * Reminder: C = F – 32 / 1.8
+ */
+
+function celsiusToFahrenheit(tem){
+    return (tem * 1.8) + 32;
+}
+
+console.log(celsiusToFahrenheit(10));
