a365c74e3c1f616a0b630c579e8e1fde9ec748ba
Mofiro Jean <mofirojean@gmail.com>
2022-08-06 21:01:07 +0100

    implemented challenge 14

--- 25 Coding Challenge I/JavaScript/Challenge14.js
+++ 25 Coding Challenge I/JavaScript/Challenge14.js
@@ -1,2 +1,17 @@
 // Challenge 14: Print the first 10 Fibonacci numbers without using recursion.
 
+let f0 = 0;
+let f1 = 1;
+
+console.log(f0);
+console.log(f1);
+
+for( let i = 2; i < 10; i++){
+   let fi = f1 + f0;
+   console.log(f1);
+   
+   // swaping
+   f0 = f1;
+   f1 = fi;
+
+}
