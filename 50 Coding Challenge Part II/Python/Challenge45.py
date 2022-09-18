20f23ce64168aca5b40c1349b962fc271ee5ee65
Mofiro Jean <mofirojean@gmail.com>
2022-09-18 22:51:24 +0100

    Created a typescript folder

---
+++ 50 Coding Challenge Part II/Python/Challenge45.py
@@ -0,0 +1,18 @@
+# Challenge 45: Create a function to calculate the sum of
+# all the numbers in a jagged array (array contains numbers or other
+# arrays of numbers on an unlimited number of levels)
+
+
+def sumArray(arr):
+    total = 0
+    for ar in arr:
+        if type(ar) in (tuple, list):
+            ar = sumArray(arr)
+        total += ar
+    return total
+
+
+# Driver Method
+if __name__ == "__main__":
+    newArr = [1, 2, [25, [23], [5, 12]], [100]]
+    print(sumArray(newArr))
