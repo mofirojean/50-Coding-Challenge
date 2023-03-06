19fb9deac87f36460308bdc75d54814d81ba014f
mofirojean <mofirojean@gmail.com>
2023-03-06 20:10:47 +0100

    completed challenge 1 using C

---
+++ 50 Coding Challenge Part I/C/challenge1.c
@@ -0,0 +1,12 @@
+// Coding Challenge 1: Print numbersfrom 1 to 10
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	for (int i = 1; i <= 10; ++i) {
+		printf("%d\n", i);
+	}
+
+	return 0;
+
+}
